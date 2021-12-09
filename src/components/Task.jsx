b27ab00e18495bedb2ac9c2733c3f2fdf784c27f
Lys <lysprestes@gmail.com>
2021-12-08 22:30:57 -0500

    Color change

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -14,7 +14,7 @@ const Task = ({ task, handleTaskClick, handleTaskDeletion }) => {
 	return (
 		<div
 			className="task-container"
-			style={task.completed ? { borderLeft: "6px solid chartreuse" } : {}}
+			style={task.completed ? { borderLeft: "6px solid #ff8c00" } : {}}
 		>
 			<div className="task-title" onClick={() => handleTaskClick(task.id)}>
 				{task.title}
